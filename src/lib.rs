d9467c00f5858d05b7dba527eddc0b275228f203
NickelAngeStudio <baphomat@nickelange.studio>
2023-04-05 18:23:54 -0400

    Finished lib.rs documentation. Fix #17. Fix #18. Fix #59. Fix #99. Fix #13. Fix #100.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 //! cfg_boost provides a [revamped syntax and macros](https://github.com/NickelAngeStudio/cfg_boost/wiki/Syntax) 
 //! to easily manage all `#[cfg]` [conditional compilation](https://doc.rust-lang.org/reference/conditional-compilation.html)
 //! predicates and parameters in one package.
+//! 
+//! See [features](https://github.com/NickelAngeStudio/cfg_boost/wiki/Features) to get the full list of features like aliases, attributes, automatic dependency tag documentation and more.
 //!
 //! ## Example
 //! **Transform this :**
@@ -49,7 +51,6 @@
 //!     }
 //! }
 //! ```
-//! <br>
 //! 
 //! [Get more examples on the wiki.](https://github.com/NickelAngeStudio/cfg_boost/wiki/Examples)
 use ts::{ generate_target_content, generate_meta_content, generate_match_content};
@@ -70,14 +71,17 @@ mod ts;
 /// Syntax tree
 mod syntax;
 
-
 /// Procedural macro used to declare resource and item outside function.
 /// 
 /// ## Description
 /// target_cfg! use a pattern syntax like [match](https://doc.rust-lang.org/rust-by-example/flow_control/match.html) 
-/// to define conditional compilation. One-to-many arms can be defined and contrary to [match_cfg!], **any matching arm WILL be included.**
-/// Because this behaviour is not [hygienic](https://doc.rust-lang.org/reference/macros-by-example.html#hygiene), 
-/// target_cfg! **CANNOT** be used in function (use [match_cfg!] inside function).
+/// to define conditional compilation outside a function. One-to-many arms can be defined and contrary to [match_cfg!], **any matching arm WILL be included**
+/// and not all cases are covered with a [wildcard](https://doc.rust-lang.org/reference/patterns.html#wildcard-pattern).
+/// 
+/// Because this behaviour is different from [match](https://doc.rust-lang.org/rust-by-example/flow_control/match.html), 
+/// target_cfg! **WILL PANIC** if used in function (use [match_cfg!] inside function instead).
+/// 
+/// **target_cfg! has no runtime cost.**
 /// 
 /// ## Syntax
 /// ```ignore
@@ -93,7 +97,7 @@ mod syntax;
 /// This feature can be deactivated. [More details here](https://github.com/NickelAngeStudio/cfg_boost/wiki/Documentation)
 /// 
 /// **BONUS :** target_cfg! can also generate those dependency tags. 
-/// <img src="https://github.com/NickelAngeStudio/cfg_boost/raw/main/img/tag.png?raw=true" width="600" height="190"><br>
+/// <img src="https://github.com/NickelAngeStudio/cfg_boost/raw/main/img/tag.png?raw=true" width="600" height="160"><br>
 /// [More details here](https://github.com/NickelAngeStudio/cfg_boost/wiki/Documentation)
 /// 
 /// ## Example
@@ -118,9 +122,9 @@ mod syntax;
 /// }
 /// 
 /// #[cfg(any(target_os="ios", target_os="android"))]
-/// compile_error!("Not supported");
+/// compile_error!("Mobile not supported");
 /// ```
-/// **Becomes this**
+/// **becomes**
 /// ```ignore
 /// target_cfg!{
 ///     !windows => {
@@ -140,7 +144,7 @@ mod syntax;
 ///         pub struct undocumented_featured {
 ///         }
 ///     },
-///     mobile => compile_error!("Not supported"),
+///     mobile => compile_error!("Mobile not supported"),
 /// }
 /// ```
 /// [More examples here.](https://github.com/NickelAngeStudio/cfg_boost/wiki/Examples)
@@ -154,6 +158,76 @@ pub fn target_cfg(item: TokenStream) -> TokenStream {
 
 
 /// Procedural macro used exclusively inside a function.
+/// 
+/// ## Description
+/// match_cfg! use a pattern syntax like [match](https://doc.rust-lang.org/rust-by-example/flow_control/match.html) 
+/// to define conditional compilation in a function.  The first matching arm is evaluated and all possible values must be covered with a [wildcard](https://doc.rust-lang.org/reference/patterns.html#wildcard-pattern).
+/// 
+/// This behaviour is the same as [match](https://doc.rust-lang.org/rust-by-example/flow_control/match.html), 
+/// thus match_cfg! can be used inside a function (while [target_cfg!] will [panic!]).
+/// 
+/// **match_cfg! has no runtime cost.**
+/// 
+/// ## Syntax
+/// ```ignore
+/// match_cfg!{
+///     !? alias* (| &)? !? value:pred* => {},
+///     !? alias* (| &)? !? value:pred* => {},
+///     _ => {}     // Wildcard arm
+/// };?
+/// ```
+/// [More details on syntax here.](https://github.com/NickelAngeStudio/cfg_boost/wiki/Syntax)
+/// 
+/// ## Example
+/// **This**
+/// ```ignore
+/// pub fn foo(){
+///     let a = {
+///         #[cfg(linux)]
+///         {
+///             10
+///         }
+///         #[cfg(windows)]
+///         {
+///             20
+///         }
+///         #[cfg(all(not(linux), not(windows)))]   // This would be a wildcard arm.
+///         {
+///             30
+///         }
+///     };
+/// 
+///     #[cfg(linux)]
+///     {
+///         println!("linux={}", a);
+///     }
+///     #[cfg(windows)]
+///     {
+///         println!("windows={}", a);
+///     }
+///     #[cfg(all(not(linux), not(windows)))]   // This would be a wildcard arm.
+///     {
+///         println!("not linux and not windows={}", a);
+///     }
+/// }
+/// ```
+/// **becomes**
+/// ```ignore
+/// pub fn foo(){
+///     let a = match_cfg!{
+///         linux => 10,
+///         windows => 20,
+///         _ => 30     // Last `,` is optional like match
+///     };
+/// 
+///     match_cfg!{
+///         linux => println!("linux={}", a),
+///         windows => println!("windows={}", a),
+///         _ => println!("not linux and not windows={}", a),
+///     };
+/// }
+/// ```
+/// [More examples here.](https://github.com/NickelAngeStudio/cfg_boost/wiki/Examples)
 #[proc_macro]
 pub fn match_cfg(item: TokenStream) -> TokenStream {
 
@@ -163,7 +237,40 @@ pub fn match_cfg(item: TokenStream) -> TokenStream {
 }
 
 
-/// Attribute macro used for one item.
+/// Attribute macro like [cfg](https://doc.rust-lang.org/rust-by-example/attribute/cfg.html) with [simplified syntax](https://github.com/NickelAngeStudio/cfg_boost/wiki/Syntax) used for one item.
+/// 
+/// ## Description
+/// meta_cfg work exactly like [cfg](https://doc.rust-lang.org/rust-by-example/attribute/cfg.html) but with [simplified syntax](https://github.com/NickelAngeStudio/cfg_boost/wiki/Syntax).
+/// 
+/// **meta_cfg has no runtime cost.**
+/// 
+/// ## Syntax
+/// ```ignore
+/// #[meta_cfg(!? alias* (| &)? !? value:pred*)]
+/// item
+/// ```
+/// [More details on syntax here.](https://github.com/NickelAngeStudio/cfg_boost/wiki/Syntax)
+/// 
+/// ## Documentation
+/// meta_cfg always wrap predicate with `doc | (predicates)` if `doc` is not defined. This allow `cargo doc` to always generate documentation. 
+/// This feature can be deactivated. [More details here](https://github.com/NickelAngeStudio/cfg_boost/wiki/Documentation)
+/// 
+/// **BONUS :** meta_cfg can also generate those dependency tags. 
+/// <img src="https://github.com/NickelAngeStudio/cfg_boost/raw/main/img/tag.png?raw=true" width="600" height="160"><br>
+/// [More details here](https://github.com/NickelAngeStudio/cfg_boost/wiki/Documentation)
+/// 
+/// ## Example
+/// **This**
+/// ```ignore
+/// #[cfg(any(doc, any(windows, unix, target_os="macos")))]
+/// pub fn foo() {}
+/// ```
+/// **becomes**
+/// ```ignore
+/// #[meta_cfg(windows | unix | macos)]
+/// pub fn foo() {}
+/// ```
+/// [More examples here.](https://github.com/NickelAngeStudio/cfg_boost/wiki/Examples)
 #[proc_macro_attribute]
 pub fn meta_cfg(attr: TokenStream, item: TokenStream) -> TokenStream {
 
